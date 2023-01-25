5a03edf31c282cdd4a907d52032f777e0c78acd2
Pavel <181095P@gmail.com>
2023-01-25 12:57:23 +0400

    Add the user registration

--- coolsite/women/views.py
+++ coolsite/women/views.py
@@ -80,3 +80,13 @@ class WomenCategory(DataMixin, ListView):
         c_def = self.get_user_context(title='Категория -' + str(context['posts'][0].cat),
                                       cat_selected=context['posts'][0].cat_id)
         return dict(list(context.items()) + list(c_def.items()))
+
+class RegisterUser(DataMixin, CreateView): #регистрация пароля
+    form_class = RegisterUserForm
+    template_name = 'women/register.html'
+    success_url = reverse_lazy('login')
+ 
+    def get_context_data(self, *, object_list=None, **kwargs):
+        context = super().get_context_data(**kwargs)
+        c_def = self.get_user_context(title="Регистрация")
+        return dict(list(context.items()) + list(c_def.items()))
